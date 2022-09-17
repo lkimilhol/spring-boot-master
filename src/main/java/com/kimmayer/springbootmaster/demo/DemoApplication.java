cff9055d64514b53eb49e79d5d644de8251977d7
lkimilhol <lkimilhol@gmail.com>
2022-09-17 15:34:03 +0900

    Initial commit

---
+++ src/main/java/com/kimmayer/springbootmaster/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.kimmayer.springbootmaster.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
